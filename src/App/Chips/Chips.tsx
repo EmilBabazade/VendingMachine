8cc020785027ca8760dbae04b56da5bcba8b0412
emilbabazade20@gmail.com <emilbabazade29@gmail.com>
2022-07-10 15:49:59 +0400

    chips

--- src/App/Chips/Chips.tsx
+++ src/App/Chips/Chips.tsx
@@ -1,8 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Chips.css';
+import { ChipsState } from './ChipsState';
 
-export default class Chips extends Component {
+export default class Chips extends Component<any, ChipsState> {
+    constructor(props: any) {
+        super(props);
+        this.state = { chipsEaten: 0 };
+    }
+
+    eat = () => {
+        this.setState({chipsEaten: this.state.chipsEaten + 1});
+    }
+
     render() {
-        return <div>dv</div>
+        const {chipsEaten} = this.state;
+        return <div className='Chips'>
+            <div className='VendingMachine-box'>
+                <div>BAGS EATEN: {chipsEaten}</div>
+                <button className='Chips-button' onClick={this.eat}>NOM NOM NOM</button>
+                <li><Link to={'/'}>GO BACK</Link></li>
+            </div>
+        </div>
     }
 }
---
+++ src/App/Chips/ChipsState.ts
@@ -0,0 +1,3 @@
+export interface ChipsState {
+    chipsEaten: number
+}
