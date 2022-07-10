f5ebd91679853afa1107f4e736af2aa2888bb642
emilbabazade20@gmail.com <emilbabazade29@gmail.com>
2022-07-10 15:33:48 +0400

    routing

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import VendingMachine from './VendingMachine/VendingMachine';
+import { Route, Routes } from 'react-router-dom';
+import Chips from './Chips/Chips';
 
 class App extends Component {
   render() {
-    return <div>
-      <VendingMachine />
-    </div>
+    return (
+      <Routes> 
+        <Route path='/' element={<VendingMachine/>} />
+        <Route path='/chips' element={<Chips/>} />
+      </Routes>
+    );
   }
 }
 
---
+++ src/App/Chips/Chips.tsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './Chips.css';
+
+export default class Chips extends Component {
+    render() {
+        return <div>dv</div>
+    }
+}
